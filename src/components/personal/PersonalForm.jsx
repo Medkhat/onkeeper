97db6767461371e50c298e844e30a6c018263e01
Medkhat <medkhat.akimbek@gmail.com>
2020-01-08 22:26:36 +0600

    Onkeeper App

--- src/components/personal/PersonalForm.jsx
+++ src/components/personal/PersonalForm.jsx
@@ -7,6 +7,25 @@ const PersonalForm = () => {
     <div className={p.personal_form}>
       <div className={p.upload_img}>
         <img src={def_user} alt="DEFAULT_USER" />
+        <label className={p.img_label}>
+          Загрузить фото
+          <input type="file" style={{ display: "none" }} />
+        </label>
+      </div>
+      <div className={p.form_input}>
+        <input type="text" placeholder="Логин..." />
+      </div>
+      <div className={p.form_input}>
+        <input type="password" placeholder="Пароль..." />
+      </div>
+      <div className={p.form_input}>
+        <input type="text" placeholder="Полное имя..." />
+      </div>
+      <div className={p.form_input}>
+        <input type="number" placeholder="Номер телефона..." />
+      </div>
+      <div className={p.form_input}>
+        <input type="email" placeholder="Email..." />
       </div>
     </div>
   )
